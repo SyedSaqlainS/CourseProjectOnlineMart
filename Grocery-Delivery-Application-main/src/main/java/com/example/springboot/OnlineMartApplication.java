93c7cdf28f0bb4d53cf28c74970405a042a15f75
SyedSaqlainS <138276044+SyedSaqlainS@users.noreply.github.com>
2023-12-14 14:36:27 +0530

    Add files via upload

---
+++ Grocery-Delivery-Application-main/src/main/java/com/example/springboot/OnlineMartApplication.java
@@ -0,0 +1,17 @@
+package com.example.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class OnlineMartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineMartApplication.class, args);
+		System.out.println("System is running");
+
+	}
+
+}
